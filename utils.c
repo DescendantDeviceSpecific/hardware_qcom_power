c9de087e2f7de4b9f436b7a9a39b15a9cac0c43f
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-03-26 01:50:35 +0000

    Silence QCOM PowerHAL failed to acquire lock

--- utils.c
+++ utils.c
@@ -248,7 +248,7 @@ void interaction(int duration, int num_args, int opt_list[])
         if (perf_lock_acq) {
             lock_handle = perf_lock_acq(lock_handle, duration, opt_list, num_args);
             if (lock_handle == -1)
-                ALOGE("Failed to acquire lock.");
+                ALOGV("Failed to acquire lock.");
         }
     }
 #endif
@@ -268,7 +268,7 @@ int interaction_with_handle(int lock_handle, int duration, int num_args, int opt
         if (perf_lock_acq) {
             lock_handle = perf_lock_acq(lock_handle, duration, opt_list, num_args);
             if (lock_handle == -1)
-                ALOGE("Failed to acquire lock.");
+                ALOGV("Failed to acquire lock.");
         }
     }
     return lock_handle;
@@ -288,7 +288,7 @@ int perf_hint_enable(int hint_id , int duration)
         if (perf_hint) {
             lock_handle = perf_hint(hint_id, NULL, duration, -1);
             if (lock_handle == -1)
-                ALOGE("Failed to acquire lock.");
+                ALOGV("Failed to acquire lock.");
         }
     }
     return lock_handle;
@@ -304,7 +304,7 @@ int perf_hint_enable_with_type(int hint_id, int duration, int type)
         if (perf_hint) {
             lock_handle = perf_hint(hint_id, NULL, duration, type);
             if (lock_handle == -1)
-                ALOGE("Failed to acquire lock.");
+                ALOGV("Failed to acquire lock.");
         }
     }
     return lock_handle;
@@ -323,7 +323,7 @@ int perform_hint_action(int hint_id, int resource_values[], int num_resources)
                 num_resources);
 
         if (lock_handle == -1) {
-            ALOGE("Failed to acquire lock.");
+            ALOGV("Failed to acquire lock.");
             return -EINVAL;
         }
 
